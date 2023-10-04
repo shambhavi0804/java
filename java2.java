b4ef10dba18f3fbea49cfe6c41d8a7e68c62373e
shambhavi0804 <137291683+shambhavi0804@users.noreply.github.com>
2023-10-04 14:32:24 +0530

    Add files via upload

---
+++ java2.java
@@ -0,0 +1,17 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+ekj
+urguer
+kjsdhf87eyt
+dkyr7i4
+kdyriu
+kruti
+dktuir
+dkutio
+,dm
